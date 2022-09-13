431cc9ee95c204e543b36e293465c03484a7ec8d
bishnuthapa1820 <bishnuthapa1820@gmail.com>
2022-09-13 21:22:03 +0545

    navbar spacing fixed

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,12 +11,39 @@ const Navbar = () => {
   if (width < 1024) return <MobileNavbar />;
 
   return (
+    <div className="flex justify-between items-center py-2">
+      <Link to="/" className="flex-none">
+        <Logo />
+      </Link>
+
+      <div className="flex items-center gap-12 xl:gap-16">
+        {navigation.map((item) => (
+          <Link
+            to={item.path}
+            key={item.id}
+            className={cn("tracking-wide font-medium text-base text-white")}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+
+      <Link to="/contact">
+        <Button className="border-[1px] border-white text-white">
+          Let's talk
+        </Button>
+      </Link>
+    </div>
+  );
+
+  return (
     <div>
-      <div className="flex items-center py-2">
-        <Link to="/">
-          <Logo className="flex-none" />
+      <div className="flex items-center justify-between py-2">
+        <Link to="/" className="flex-none">
+          <Logo />
         </Link>
-        <div className="grow flex items-center justify-end">
+
+        <div className="flex justify-end">
           <div className="inline-block">
             <div className="flex items-center space-x-12 lg:space-x-16">
               {navigation.map((item) => (
@@ -30,15 +57,16 @@ const Navbar = () => {
                   {item.name}
                 </Link>
               ))}
-              <div className="pl-20 xl:pl-24 2xl:pl-32">
-                <Link to="/contact">
-                  <Button className="border-[1px] border-white text-white">
-                    Let's talk
-                  </Button>
-                </Link>
-              </div>
             </div>
           </div>
+        </div>
+
+        <div className="pl-20 xl:pl-24 2xl:pl-32">
+          <Link to="/contact">
+            <Button className="border-[1px] border-white text-white">
+              Let's talk
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
