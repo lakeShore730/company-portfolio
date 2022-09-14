5fde7e6a27215665bf679e6acc5d557093a21f6e
bishnuthapa1820 <bishnuthapa1820@gmail.com>
2022-09-14 11:23:15 +0545

    b

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -35,42 +35,6 @@ const Navbar = () => {
       </Link>
     </div>
   );
-
-  return (
-    <div>
-      <div className="flex items-center justify-between py-2">
-        <Link to="/" className="flex-none">
-          <Logo />
-        </Link>
-
-        <div className="flex justify-end">
-          <div className="inline-block">
-            <div className="flex items-center space-x-12 lg:space-x-16">
-              {navigation.map((item) => (
-                <Link
-                  to={item.path}
-                  key={item.id}
-                  className={cn(
-                    "tracking-wide font-medium text-base text-white"
-                  )}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div className="pl-20 xl:pl-24 2xl:pl-32">
-          <Link to="/contact">
-            <Button className="border-[1px] border-white text-white">
-              Let's talk
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default Navbar;
