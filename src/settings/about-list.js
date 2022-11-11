4e8338ee13229d524858c79882d934f5eaf170e0
Your Name <you@example.com>
2022-11-11 13:49:12 -0600

    x

--- src/settings/about-list.js
+++ src/settings/about-list.js
@@ -1,21 +1,21 @@
 const aboutList = [
   {
-    title: "A well plan",
+    title: "A well laid plan",
     description:
       "Dummy text is also used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical. used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical.",
   },
   {
-    title: "A well plan",
+    title: "Follow you ambitions",
     description:
       "Dummy text is also used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical. used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical.",
   },
   {
-    title: "A well plan",
+    title: "Explore your curiosity",
     description:
       "Dummy text is also used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical. used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical.",
   },
   {
-    title: "A well plan",
+    title: "Enjoy the process",
     description:
       "Dummy text is also used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical. used to demonstrate the appearance of different typefaces and layouts, and in general the content of dummy text is nonsensical.",
   },
